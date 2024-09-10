bc4d7d21df5f76f90335f24026ede24eb92b9d4d
Leon Franken <leon.franken@funkemedien.de>
2024-09-10 15:48:58 +0200

    fix: button hover and active

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,7 +5,7 @@ export const Button = ({
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & { negative?: boolean }) => {
   return (
     <button
-      className={`inline-flex items-center justify-center rounded-full border bg-indigo-950 text-xl font-bold leading-none text-white ${negative ? 'border-rose-900' : 'border-indigo-800'} min-w-24 px-3 py-2`}
+      className={`inline-flex items-center justify-center rounded-full border bg-indigo-950 text-xl font-bold leading-none text-white hover:bg-indigo-950/50 active:bg-indigo-900 ${negative ? 'border-rose-900' : 'border-indigo-800'} min-w-24 px-3 py-2`}
       {...props}
     >
       <span>{children}</span>
@@ -22,7 +22,7 @@ export const IconButton = ({
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & { negative?: boolean; large?: boolean; auto?: boolean }) => {
   return (
     <button
-      className={`inline-flex items-center justify-center rounded-full border bg-indigo-950 text-xl leading-none text-white ${negative ? 'border-rose-900' : 'border-indigo-800'} ${auto ? 'px-3 py-1.5' : large ? 'h-12 w-12' : 'h-10 w-10'}`}
+      className={`inline-flex items-center justify-center rounded-full border bg-indigo-950 text-xl leading-none text-white hover:bg-indigo-950/50 active:bg-indigo-900 ${negative ? 'border-rose-900' : 'border-indigo-800'} ${auto ? 'px-3 py-1.5' : large ? 'h-12 w-12' : 'h-10 w-10'}`}
       {...props}
     >
       <span>{children}</span>
