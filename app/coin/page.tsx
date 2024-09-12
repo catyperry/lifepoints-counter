5f556ca65671dabdfb95b1437eb226ffaa2c8921
Leon Franken <leon.franken@funkemedien.de>
2024-09-12 21:19:47 +0200

    fix(Coin): work on physic parameters and ux and coin textures

--- app/coin/page.tsx
+++ app/coin/page.tsx
@@ -3,52 +3,73 @@
 import React from 'react';
 import { Canvas, useThree } from '@react-three/fiber';
 import { Physics, useCylinder, usePlane, Triplet } from '@react-three/cannon';
-import * as THREE from 'three';
 import { useTexture } from '@react-three/drei';
+import { IconButton } from '../components/Button';
+import Image from 'next/image';
+import { Mesh } from 'three';
 
-// Main Canvas Scene
 export default function Page() {
+  const [flipCoin, setFlipCoin] = React.useState(false);
+
   return (
-    <div className="h-dvh w-dvw">
-      <CoinflipApp />
+    <div className="relative h-dvh w-dvw">
+      <div className="relative flex h-full w-full items-center justify-center bg-gradient-to-b from-green-200 to-green-500">
+        {flipCoin ? (
+          <div className="absolute left-0 top-0 h-full w-full">
+            <CoinFlip setFlipCoin={setFlipCoin} />
+          </div>
+        ) : (
+          <IconButton onClick={() => setFlipCoin(true)}>
+            <div className="">
+              <Image src="/AnubisCoin_Front.webp" width="1000" height="1000" alt="Coin with Anubis displayed" />
+            </div>
+          </IconButton>
+        )}
+      </div>
     </div>
   );
 }
 
-//  we can use a random start rotation for the coin to make it more random
-
-// Coin component with physics
 const Coin: React.FC = () => {
-  const textureTop = useTexture('/baphomet.jpg');
-  const textureBottom = useTexture('/blueeyeswhitedragon.webp');
-  // Apply physics to the coin (cylinder)
-  const coinSize = 0.3;
-  const [coinRef, api] = useCylinder(() => ({
-    mass: 0.01,
+  const textureFront = useTexture('/AnubisCoin_Front.webp');
+  const textureBack = useTexture('/AnubisCoin_Back.webp');
+  const coinSize = 0.4;
+
+  const [coinRef, api] = useCylinder<Mesh>(() => ({
+    mass: 0.1,
     position: [0, 0, 0] as Triplet,
-    args: [coinSize, coinSize, 0.03, 32], // Cylinder shape for the coin
-    rotation: [Math.PI / 2, 0, 0] as Triplet, // Initial rotation
+    args: [coinSize, coinSize, 0.04, 32], // Cylinder shape for the coin
+    rotation: [((Math.random() < 0.5 ? -1 : 1) * Math.PI) / 2, Math.PI / 2, 0] as Triplet, // Initial rotation
   }));
+
+  const flipCoin = React.useCallback(() => {
+    const randomSpinX = randomFloat(2.5, 7) * (Math.random() < 0.5 ? -1 : 1);
+    const randomSpinY = randomFloat(2.5, 7) * (Math.random() < 0.5 ? -1 : 1);
+    const randomSpinZ = randomFloat(0.5, 1.5) * (Math.random() < 0.5 ? -1 : 1);
+    const zImpulse = randomFloat(0.85, 1.0);
+    api.applyImpulse([0, 0, zImpulse], [0, 0, 0]);
+    api.applyTorque([randomSpinX, randomSpinY, randomSpinZ]);
+  }, [api]);
 
   const { viewport } = useThree();
 
   // Create invisible "walls" for the screen bounds
-  const [leftWallRef] = usePlane(() => ({
+  const [leftWallRef] = usePlane<Mesh>(() => ({
     position: [-viewport.width / 2, 0, 0],
     rotation: [0, Math.PI / 2, 0], // rotate the plane to face the coin
   }));
 
-  const [rightWallRef] = usePlane(() => ({
+  const [rightWallRef] = usePlane<Mesh>(() => ({
     position: [viewport.width / 2, 0, 0],
     rotation: [0, -Math.PI / 2, 0],
   }));
 
-  const [topWallRef] = usePlane(() => ({
+  const [topWallRef] = usePlane<Mesh>(() => ({
     position: [0, viewport.height / 2, 0],
     rotation: [Math.PI / 2, 0, 0], // rotate the plane to face the coin from the top
   }));
 
-  const [bottomWallRef] = usePlane(() => ({
+  const [bottomWallRef] = usePlane<Mesh>(() => ({
     position: [0, -viewport.height / 2, 0],
     rotation: [-Math.PI / 2, 0, 0],
   }));
@@ -57,25 +78,26 @@ const Coin: React.FC = () => {
     // You can dynamically update walls if the window resizes
   }, [viewport.width, viewport.height]);
 
-  const flipCoin = () => {
-    console.log('flipCoin');
-    api.position.set(0, 0, 0); // Reset coin's position
-    api.velocity.set(0, 0, randomFloat(7, 10)); // Apply upward velocity to simulate flip
-    api.angularVelocity.set(
-      randomFloat(15, 35) * (Math.random() < 0.5 ? -1 : 1), // Randomize spin on X axis
-      randomFloat(15, 35) * (Math.random() < 0.5 ? -1 : 1), // Randomize spin on Y axis
-      0,
-    );
-  };
+  React.useEffect(() => {
+    flipCoin();
+  }, [flipCoin]);
 
   return (
     <group>
       {/* Coin Mesh */}
-      <mesh ref={coinRef} receiveShadow castShadow onClick={() => flipCoin()}>
-        <cylinderGeometry args={[coinSize, coinSize, 0.03, 32]} />
-        <meshStandardMaterial color="#ff44ff" attach="material-0" />
-        <meshStandardMaterial map={textureTop} attach="material-1" />
-        <meshStandardMaterial map={textureBottom} attach="material-2" />
+      <mesh
+        ref={coinRef}
+        receiveShadow
+        castShadow
+        onClick={(event) => {
+          flipCoin();
+          event.stopPropagation();
+        }}
+      >
+        <cylinderGeometry args={[coinSize, coinSize, 0.04, 32]} />
+        <meshStandardMaterial color="#68563f" attach="material-0" />
+        <meshStandardMaterial map={textureFront} attach="material-1" />
+        <meshStandardMaterial map={textureBack} attach="material-2" />
       </mesh>
 
       <mesh ref={leftWallRef} visible={false} />
@@ -87,33 +109,31 @@ const Coin: React.FC = () => {
 };
 
 // Ground component with physics
-const Ground: React.FC = () => {
-  const [ref] = usePlane(() => ({
+const Ground = ({ onClick }: { onClick: () => void }) => {
+  const [ref] = usePlane<Mesh>(() => ({
     mass: 10,
-    rotation: [0, 0, 0] as Triplet, // Flat ground
-    position: [0, 0, 0] as Triplet,
+    rotation: [0, 0, 0] as Triplet,
+    position: [0, 0, -0.5] as Triplet,
     type: 'Static',
   }));
 
   return (
-    <mesh ref={ref} receiveShadow>
-      <planeGeometry args={[10, 10]} />
-      {/* <shadowMaterial opacity={0.3} />
-       */}
-      <meshStandardMaterial side={THREE.FrontSide} />
+    <mesh ref={ref} receiveShadow onClick={onClick}>
+      <planeGeometry args={[100, 100]} />
+      <shadowMaterial transparent opacity={0.3} />
     </mesh>
   );
 };
 
-const CoinflipApp = () => {
+const CoinFlip = ({ setFlipCoin }: { setFlipCoin: (active: boolean) => void }) => {
   return (
-    <Canvas shadows>
+    <Canvas shadows gl={{ alpha: true }} className="h-dvh w-dvw border-none bg-transparent text-transparent">
       <ambientLight intensity={1} />
       <directionalLight position={[0, 5, 5]} intensity={1} castShadow />
       <Physics gravity={[0, 0, -9.8]}>
         {/* Simulate Earth's gravity */}
         <Coin />
-        <Ground />
+        <Ground onClick={() => setFlipCoin(false)} />
       </Physics>
       {/* <OrbitControls /> */}
     </Canvas>
@@ -121,5 +141,5 @@ const CoinflipApp = () => {
 };
 
 function randomFloat(min: number, max: number) {
-  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+  return parseFloat((Math.random() * (max - min) + min).toFixed(4));
 }
